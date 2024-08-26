f143b7a2524b958d2d8eb26610db5e1e2b9e40ea
Kyle Finley <kyle@finley.sh>
2024-08-26 15:25:07 -0400

    remove `log-*` functions & most `_STYLE_*` env vars

--- src/dot_zshrc.d/000-color-functions.sh
+++ src/dot_zshrc.d/000-color-functions.sh
@@ -2,31 +2,7 @@
 #
 # Functions & environment variables that can be used to print colored messages.
 
-export _STYLE_BOLD="\e[1m";
-export _STYLE_DIM="\e[2m";
-export _STYLE_ITALIC="\e[3m";
-export _STYLE_UNDERLINE="\e[4m";
-
-export _STYLE_BLACK="\e[30m";
-export _STYLE_BLACK_BOLD="\e[30;1m";
-
-export _STYLE_RED="\e[31m";
 export _STYLE_RED_BOLD="\e[31;1";
-
-export _STYLE_GREEN="\e[32m";
-export _STYLE_GREEN_BOLD="\e[32;1m";
-
-export _STYLE_YELLOW="\e[33m";
-export _STYLE_YELLOW_BOLD="\e[33;1m";
-
-export _STYLE_BLUE="\e[34m";
-export _STYLE_BLUE_BOLD="\e[34;1m";
-
-export _STYLE_MAGENTA="\e[35m";
-export _STYLE_MAGENTA_BOLD="\e[35;1m";
-
-export _STYLE_CYAN="\e[36m";
-export _STYLE_CYAN_BOLD="\e[36;1m";
 
 export _STYLE_RESET="\e[0m";
 
@@ -74,10 +50,3 @@ secho() {
 
   echo -e "${color}m${text}${_STYLE_RESET}";
 }
-
-function log-debug { secho "[DEBUG] ${1}" cyan dim italic; }
-function log-deprecated { secho "[DEPRECATED] ${1}" cyan bold; }
-function log-error { secho "[ERROR] ${1}" red bold; }
-function log-notice { secho "[NOTICE] ${1}" magenta bold; }
-function log-success { secho "[SUCCESS] ${1}" green bold; }
-function log-warning { secho "[WARNING] ${1}" yellow bold; }
