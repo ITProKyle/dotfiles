4e01d8b4cc3afe7afe0ab0f02321efd66bf6d01b
Kyle Finley <kyle@finley.sh>
2024-10-15 10:46:48 -0400

    bump python & deps versions, add keybinds

--- src/dot_zshrc.d/999-keybinds.sh
+++ src/dot_zshrc.d/999-keybinds.sh
@@ -4,3 +4,5 @@
 
 bindkey "^[[H" beginning-of-line  # HOME
 bindkey "^[[F" end-of-line  # END
+bindkey "^[^[[C" forward-word  # macOS `Alt + ->` or `Option + ->` depending on keyboard (linux might need "^[[1;3C")
+bindkey "^^[^[[D" backward-word  # macOS `Alt + <-` or `Option + <-` depending on keyboard (linux might need "^[[1;3D")
